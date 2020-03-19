2d79804e85bc34bd0204e0dcbd86c2adb8625654
Aswin Behera <aswinkumarbehera@gmail.com>
2020-03-19 18:16:54 +0530

    added modified data

--- src/components/infotypes.js
+++ src/components/infotypes.js
@@ -1,11 +1,15 @@
 import React from "react";
-import {infoData} from "./data/infoTypeData";
-import { v4 as uuidv4 } from 'uuid';
+import { infoData } from "./data/infoTypeData";
+import { v4 as uuidv4 } from "uuid";
 
 const InfoTypes = () => {
   const infoTypeList = infoData.map(infotype => {
-      console.log(infotype);
-    return <div key={uuidv4()} className="infoType">{infotype}</div>;
+    
+    return (
+      <div key={uuidv4()} className="infoType">
+        {infotype}
+      </div>
+    );
   });
 
   return <div className="infoTypeCon">{infoTypeList}</div>;
