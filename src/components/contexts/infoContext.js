4a8823bed327ee59901e454bd986faedf9463874
Aswin Behera <aswinkumarbehera@gmail.com>
2020-07-02 15:49:52 +0530

    logic correction

--- src/components/contexts/infoContext.js
+++ src/components/contexts/infoContext.js
@@ -1,28 +1,35 @@
-import React, {useState, createContext} from 'react';
-
+import React, { useState, createContext, useEffect, useContext } from 'react';
+import { ResourceContext } from './resourceContext';
 export const InfoContext = createContext();
-
 const InfoContextProvider = (props) => {
-     const [resType, setResType] = useState("All");
-
-     const addResType = (res, type)=> {
-         let results;
-         if (type !== "All") {
-            results = res.filter(content => 
-                content.infotype === type
-            )
-         }else {
-             results = res;
-         }
-    
-        setResType(results);
-     }
-
-    return ( 
-        <InfoContext.Provider value = {{resType, addResType}}>
-            {props.children}
-        </InfoContext.Provider>
-     );
-}
- 
+  const { res: resource } = useContext(ResourceContext);
+  const [resType, setResType] = useState(resource);
+  const [currentInfoType, setCurrentInfoType] = useState('All');
+  useEffect(() => {
+    setResType(() => {
+      if (currentInfoType !== 'All') {
+        return resource.filter(
+          (content) => content.infotype === currentInfoType
+        );
+      }
+      return resource;
+    });
+  }, [resource, currentInfoType]);
+  const addResType = (res, type) => {
+    let results;
+    if (type !== 'All') {
+      results = res.filter((content) => content.infotype === type);
+    } else {
+      results = res;
+    }
+    console.log(results);
+    setCurrentInfoType(type);
+    setResType(results);
+  };
+  return (
+    <InfoContext.Provider value={{ resType, addResType }}>
+      {props.children}
+    </InfoContext.Provider>
+  );
+};
 export default InfoContextProvider;
